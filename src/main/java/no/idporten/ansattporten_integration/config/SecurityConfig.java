c4aa4eb68cd04e4e8f3128008cc3c51e7f11e870
Magnus Løwer <magnus.lower@gmail.com>
2024-07-18 09:01:56 +0200

    Added comments to src/main/java/no.idporten.ansattporten_integration/config/SecurityConfig for clarity

--- src/main/java/no/idporten/ansattporten_integration/config/SecurityConfig.java
+++ src/main/java/no/idporten/ansattporten_integration/config/SecurityConfig.java
@@ -11,15 +11,25 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+/**
+ * Configuration class for setting up security configurations for the application.
+ */
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    /**
+     * Configures the security filter chain for the application.
+     *
+     * @param http the ServerHttpSecurity instance to configure
+     * @return the configured SecurityWebFilterChain
+     */
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-			//Disable csrf requirement, men må skrus på i produksjon
+			//Disable csrf requirement, should be enabled in production
 			.csrf((csrf) -> csrf.disable());
+        // Build and return the configured security filter chain
         return http.build();
     }    
 }
