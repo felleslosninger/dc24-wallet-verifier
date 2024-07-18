91aa69532b86b4dc8e30e52876273200326a33fd
Magnus Løwer <magnus.lower@gmail.com>
2024-07-18 10:35:16 +0200

    Formatted the file according to suggestions from SonarLint

--- src/main/java/no/idporten/ansattporten_integration/config/SecurityConfig.java
+++ src/main/java/no/idporten/ansattporten_integration/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package no.idporten.ansattporten_integration.config;
 
-import java.util.function.Consumer;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 /**
  * Configuration class for setting up security configurations for the application.
@@ -28,7 +23,7 @@ public class SecurityConfig {
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
 			//Disable csrf requirement, should be enabled in production
-			.csrf((csrf) -> csrf.disable());
+			.csrf(ServerHttpSecurity.CsrfSpec::disable);
         // Build and return the configured security filter chain
         return http.build();
     }    
